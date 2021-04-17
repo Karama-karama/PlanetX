dbab85970a23d58c18cb9bf179b3e49a2c0139bf
Karama Aissaoui <aissaoui.karama@gmail.com>
2021-04-17 01:38:40 +0100

    Initial commit

---
+++ Assets/Scripts/Movescenesaturne1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movescenesaturne1 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Neptune"))
+        {
+            SceneManager.LoadScene(14);
+        }
+
+    }
+}  
---
+++ Assets/aleatoire.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aleatoire : MonoBehaviour
+{
+        public Rigidbody2D Comet;
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+       Vector3 PositionComet = new Vector3 (Random.Range (-5,5), Random.Range(-5,5),2); 
+      Comet = Instantiate (Comet, PositionComet, Quaternion.identity) as Rigidbody2D;
+      print(PositionComet);  
+    }
+
+   
+}
